27e51d8f0c000ab83adbc604d9aaf74e99054fc1
Josh.London <josh.london@noaa.gov>
2021-05-07 09:25:08 -0700

    ADDED ADDITIONAL ADFG DATA SOURCES
    up to this point, some ADFG data sources were not included in the analysis. This is the initial attempt to include those data.

--- R/get_locs_sf.R
+++ R/get_locs_sf.R
@@ -17,8 +17,8 @@ get_locs_sf <- function() {
   con <- dbConnect(
     odbc(),
     dsn = "PostgreSQL pep",
-    uid = get_kc_account("pgpep_londonj"),
-    pwd = decrypt_kc_pw("pgpep_londonj")
+    uid = keyringr::get_kc_account("pgpep_londonj"),
+    pwd = keyringr::decrypt_kc_pw("pgpep_londonj")
   )
 
   deployments_db <- tbl(con, in_schema("telem","tbl_tag_deployments")) %>%
@@ -26,13 +26,16 @@ get_locs_sf <- function() {
 
   spenos_db <- tbl(con, in_schema("capture","for_telem")) %>% collect()
 
-  locs_qry <- "SELECT deployid,
-                locs_dt,
-                type,
-                quality,
-                error_radius,
-                geom
-              FROM telem.geo_wc_locs_qa"
+  locs_qry <- "SELECT deployid, type, error_radius, locs_dt, quality, geom
+              FROM telem.geo_wc_locs_qa
+              UNION ALL
+              SELECT deployid, NULL AS type, NULL AS error_radius,
+                     loc_dt as locs_dt, argos_lc as quality, loc_geom as geom
+              FROM telem.adfg_locs
+              UNION ALL
+              SELECT deployid::VARCHAR, NULL AS type, NULL AS error_radius,
+                     loc_dt as locs_dt, argos_lc as quality, loc_geom as geom
+              FROM telem.nsb_locs;"
 
   locs_sf <- read_sf(con, query = locs_qry) %>%
     left_join(deployments_db, by = 'deployid') %>%
