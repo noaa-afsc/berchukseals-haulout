e1095d463b2448a335306e3e332775d03563ab02
Josh M. London <josh.london@noaa.gov>
2023-08-10 15:15:46 -0700

    INITIAL ADD OF mgcv MODEL FIT
    just testing for now

--- R/fit_spotted_models.R
+++ R/fit_spotted_models.R
@@ -20,6 +20,17 @@ fit_spotted <- function(HO_spotted) {
            group.vec = "ar1_id")
 }
 
+fit_spotted_bam <- function(HO_spotted) {
+  mgcv::bam(
+    dry ~ age_sex + s(ar1_id, bs = "re") + sin1 + cos1 + sin2 + cos2 + sin3 + cos3 + day + day2+ day3 + temp2 + wind + pressure + precip + wind*temp2 +
+      sin1*day + cos1*day + sin2*day + cos2*day + sin3*day + cos3*day +
+      sin1*day2 + cos1*day2 + sin2*day2 + cos2*day2 + sin3*day2 + cos3*day2 +
+      age_sex:day + age_sex:day2 + age_sex:day3,
+    data = HO_spotted,
+    discrete = TRUE
+  )
+}
+
 fit_spotted_noagesex <- function(HO_spotted) {
   glmmLDTS(fixed.formula = dry ~ sin1 + cos1 + sin2 + cos2 + sin3 + cos3 + day + day2+ day3 + temp2 + wind + pressure + precip + wind*temp2 +
              sin1*day + cos1*day + sin2*day + cos2*day + sin3*day + cos3*day +
